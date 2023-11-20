1f686b45785b8bd99015eb97c43171d63b2f0fe2
Arpit Kumar Yadav <72245696+arpit9377@users.noreply.github.com>
2023-11-20 19:21:56 +0530

    Update App.js
    New Routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Page from "./Components/Page";
 import Rockets from "./Components/Rockets";
 import About from "./Components/About";
 import RocketDetail from "./Components/RocketDetail";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Page />} />
+        <Route path="/page" element={<Page />} />
         <Route path="/rockets" element={<Rockets />} />
         <Route path="/rockets/:rocketId" element={<RocketDetail />} />
         <Route path="/about" element={<About />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         
       </Routes>
     </Router>
